632ef30a3053ce0b88e0d7203fc275841f2d10eb
Alexey ALERT Rubashёff <alexey.rubasheff@gmail.com>
2024-12-19 11:09:25 +0200

    ! precautions

--- instagram_rss/instagram_user_rss.py
+++ instagram_rss/instagram_user_rss.py
@@ -205,10 +205,30 @@ class InstagramUserRSS:
         reels_limit=constants.REELS_LIMIT_DEFAULT,
         tagged_limit=constants.TAGGED_LIMIT_DEFAULT,
     ):
-        posts = self.profile.get_posts() if posts else None
-        reels = self.profile.get_reels() if reels else None
-        stories = self.il.get_stories([self.profile.userid]) if stories else None
-        tagged = self.profile.get_tagged_posts() if tagged else None
+        try:
+            posts = self.profile.get_posts() if posts else None
+        except Exception as e:  # noqa: BLE001
+            LOG.error(f"Error getting posts for {self.profile.userid}: {e}")  # noqa: TRY400
+            posts = None
+
+        try:
+            reels = self.profile.get_reels() if reels else None
+        except Exception as e:  # noqa: BLE001
+            LOG.error(f"Error getting reels for {self.profile.userid}: {e}")  # noqa: TRY400
+            reels = None
+
+        try:
+            stories = self.il.get_stories([self.profile.userid]) if stories else None
+        except Exception as e:  # noqa: BLE001
+            LOG.error(f"Error getting stories for {self.profile.userid}: {e}")  # noqa: TRY400
+            stories = None
+
+        try:
+            tagged = self.profile.get_tagged_posts() if tagged else None
+        except Exception as e:  # noqa: BLE001
+            LOG.error(f"Error getting tagged posts for {self.profile.userid}: {e}")  # noqa: TRY400
+            tagged = None
+
         return self.generate_rss_feed(
             posts=posts,
             posts_limit=posts_limit,
